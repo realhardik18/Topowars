5da64de5ebbee4145d9ad8296eb7023bd95b5070
realhardik18 <realhardik18@gmail.com>
2022-05-12 17:27:48 +0530

    added website files

---
+++ website/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request
+from threading import Thread
+import requests
+
+app = Flask('')
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/leaderboard')
+def leaderboard():
+    return render_template("leaderboard.html")
+
+
+@app.route('/commands')
+def commands():
+    return render_template("commands.html")
+
+
+def run():
+    app.run(host='0.0.0.0', debug=True)
+
+
+def show_site():
+    t = Thread(target=run)
+    t.start()
+
+
+run()
+# show user stats
+# country stats
+# home page
+
+# add animations all all nic enice
